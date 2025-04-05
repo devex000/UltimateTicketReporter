bcf81657589fecd98ec5e84f5bbfff4634acc1cb
devex000 <devex8293@gmail.com>
2025-04-06 00:48:18 +0200

    Fix gui

--- resources/views/livewire/ticket-create-form.blade.php
+++ resources/views/livewire/ticket-create-form.blade.php
@@ -5,35 +5,48 @@
     @endif
 
     <form wire:submit.prevent="submit">
-        <div>
-            <label for="problem_since">Problem since</label><br>
-            <input type="datetime-local" wire:model="problem_since" id="problem_since">
-        </div>
-
-        <div>
-            <label for="description">Description</label><br>
-            <textarea wire:model="description" id="description" cols="30" rows="10"></textarea>
-        </div>
-
-        <div>
-            <label for="priority_id">Priority</label><br>
-            <select wire:model.live="priority_id" id="priority_id">
-                <option value="">-- select priority --</option>
-                @foreach ($priorities as $priority)
-                    <option value="{{ $priority->id }}">{{ $priority->name }}</option>
-                @endforeach
-            </select>
-        </div>
-
-        <div>
-            <label for="category_id">Category</label><br>
-            <select wire:model.live="category_id" id="category_id">
-                <option value="">-- select category --</option>
-                @foreach ($categories as $category)
-                    <option value="{{ $category->id }}">{{ $category->name }}</option>
-                @endforeach
-            </select>
-        </div>
+        <table>
+            <tr>
+                <td>
+                    <label for="problem_since">Problem since</label>
+                </td>
+                <td>
+                    <input type="datetime-local" wire:model="problem_since" id="problem_since">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="priority_id">Priority</label>
+                </td>
+                <td>
+                    <select wire:model.live="priority_id" id="priority_id">
+                        <option value="">-- select priority --</option>
+                        @foreach ($priorities as $priority)
+                            <option value="{{ $priority->id }}">{{ $priority->name }}</option>
+                        @endforeach
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="category_id">Category</label>
+                </td>
+                <td>
+                    <select wire:model.live="category_id" id="category_id">
+                        <option value="">-- select category --</option>
+                        @foreach ($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <label for="description">Description</label><br>
+                    <textarea wire:model="description" id="description" cols="50" rows="10"></textarea>
+                </td>
+            </tr>
+        </table>
 
         <hr>
         <p>Info</p>
